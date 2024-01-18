4d1ea4d25f0f4486696b162d9d3fd384885ac75b
Tomas Nemec <owl@gtms.dev>
2024-01-18 08:10:44 +0100

    feat(errors): start migrating errors

---
+++ lib/src/errors.dart
@@ -0,0 +1,22 @@
+enum AngularWarningCode {
+  /// An error for when a directive does not define a "selector".
+  argumentSelectorMissing('Argument "selector" missing'),
+
+  /// An error for when the provided selector cannot be parsed.
+  cannotParseSelector('Cannot parse the given selector ({0})'),
+
+  /// An error for when a template points to a missing html file.
+  referencedHtmlFileDoesntExist('The referenced HTML file doesn\'t exist'),
+
+  /// An error for when a @Component has both a template and a templateUrl
+  /// defined at once.
+  templateUrlAndTemplateDefined('Cannot define both template and templateUrl. Remove one'),
+
+  /// An error for when a @Component does not have a template or a templateUrl.
+  noTemplateUrlOrTemplateDefined('Either a template or templateUrl is required'),
+  ;
+
+  final String message;
+
+  const AngularWarningCode(this.message);
+}
