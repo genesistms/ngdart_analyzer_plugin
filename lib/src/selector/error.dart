d068a44f857754dbd2c89bed32ae0e3e0e3cc325
Tomas Nemec <owl@gtms.dev>
2024-02-02 08:10:45 +0100

    feat(errors): non-nullable offset for selector errors

--- lib/src/selector/error.dart
+++ lib/src/selector/error.dart
@@ -1,5 +1,6 @@
 /// A type of [FormatException] specific to errors parsing [Selector]s.
 class SelectorParseError extends FormatException {
   int length;
-  SelectorParseError(String message, String source, int offset, this.length) : super(message, source, offset);
+  int offset;
+  SelectorParseError(String message, String source, this.offset, this.length) : super(message, source, offset);
 }
